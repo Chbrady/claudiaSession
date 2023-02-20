e997e555056a1d64a70435dbf453701a62dcd5f5
Chbrady <101840055+Chbrady@users.noreply.github.com>
2023-02-20 11:03:31 -0800

    new push

---
+++ session_2/dataTypes.js
@@ -0,0 +1,10 @@
+let float_value = 1.05 // Math.ceil(float_val) -> 2
+let number_value = 1
+let string = 'claudia'
+let object = {
+  name: 'claudia',
+  age: 32,
+  city: 'seattle'
+}
+
+console.log(typeof object)
